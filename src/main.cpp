faaeaf84e8c32497f6fcd62604525537d5297e47
chenxingyusean <chenxingyusean@163.com>
2019-07-22 17:33:44 +0800

    cudaDeviceSynchronize

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <detector.h>
 #include <uart.h>
 
+#include <cuda_runtime.h>
+
 #include <torch/script.h>
 #include <torch/torch.h>
 #include <opencv2/opencv.hpp>
@@ -19,11 +21,8 @@
 
 int main(int argc, const char* argv[]) {
     // load models
-    // torch::Device device(torch::cuda::is_available() ? torch::kCUDA : torch::kCPU);
     torch::NoGradGuard no_grad_guard;
-    std::shared_ptr<torch::jit::script::Module> netG = torch::jit::load("../models/netG.pt");
-    netG->to(at::kCUDA);
-    std::shared_ptr<torch::jit::script::Module> SSD = torch::jit::load("../models/SSD320.pt");
+    std::shared_ptr<torch::jit::script::Module> SSD = torch::jit::load("../models/G_SSD_320.pt");
     SSD->to(at::kCUDA);
 
     // load detector
@@ -56,18 +55,16 @@ int main(int argc, const char* argv[]) {
         auto img_tensor = torch::from_blob(img_float.data, {1, ssd_dim, ssd_dim, 3}).to(torch::kCUDA);
         img_tensor = img_tensor.permute({0, 3, 1, 2});
         clock_t t2 = clock();
-        netG_input.push_back(img_tensor);
-        fake_B = netG->forward(netG_input).toTensor();
-        netG_input.pop_back();
-        clock_t t3 = clock();
-        SSD_input.push_back(fake_B.add(1.0).div(2.0).mul(255.0).sub(128.));
+        SSD_input.push_back(img_tensor);
         SSD_output = SSD->forward(SSD_input).toTuple()->elements();
+        cudaDeviceSynchronize();
         SSD_input.pop_back();
         clock_t t4 = clock();
-        loc = SSD_output.at(0).toTensor().to(torch::kCPU);
-        conf = SSD_output.at(1).toTensor().to(torch::kCPU);
+        fake_B = SSD_output.at(0).toTensor();
+        loc = SSD_output.at(1).toTensor().to(torch::kCPU);
+        conf = SSD_output.at(2).toTensor().to(torch::kCPU);
+        ota_feature = SSD_output.at(3).toTensor().to(torch::kCPU);
         clock_t t5 = clock();
-//        ota_feature = SSD_output.at(2).toTensor().to(torch::kCPU);
         detections = Detect.detect(loc, conf, conf_thresh);
         clock_t t6 = clock();
         img_vis = tensor2im(fake_B);
@@ -76,13 +73,10 @@ int main(int argc, const char* argv[]) {
         std::cout << "total: " << (t7 - t1) * 1.0 / CLOCKS_PER_SEC * 1000
                   <<  ", pre: " << (t1_1 - t1) * 1.0 / CLOCKS_PER_SEC * 1000
                   <<  ", cpu2gpu: " << (t2 - t1_1) * 1.0 / CLOCKS_PER_SEC * 1000
-                  <<  ", netG: " << (t3 - t2) * 1.0 / CLOCKS_PER_SEC * 1000
-                  <<  ", SSD: "  << (t4 - t3) * 1.0 / CLOCKS_PER_SEC * 1000
+                  <<  ", G_SSD: "  << (t4 - t2) * 1.0 / CLOCKS_PER_SEC * 1000
                   <<  ", gpu2cpu: "  << (t5 - t4) * 1.0 / CLOCKS_PER_SEC * 1000
                   <<  ", detection: "  << (t6 - t5) * 1.0 / CLOCKS_PER_SEC * 1000
                   <<  ", vis: "  << (t7 - t6) * 1.0 / CLOCKS_PER_SEC * 1000 << std::endl;
 
-//        cv::imshow("test", img_vis);
-//        cv::waitKey(1);
     }
 }
