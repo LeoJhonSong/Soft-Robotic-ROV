60b741434fae9f13eddc3d5f5cd2d8eef1ee804f
Leo-AERO15WB <LeoJhon.Song@outlook.com>
2021-09-07 18:03:29 +0800

    feat(utils): improve delay test to multi tries

--- utils/delay_test.py
+++ utils/delay_test.py
@@ -10,15 +10,24 @@ import sys
 import time
 import socket
 
+PORT = 9000
+count = 100
+
 if sys.argv[1] == 'client':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((sys.argv[2], 8000))
-        s.send(bytes(str(time.time()).encode()))
-        print(f'{(time.time() - float(s.recv(1024).decode("utf-8"))) * 1000:.03f}ms')
+        s.connect((sys.argv[2], PORT))
+        sum = 0
+        for i in range(count):
+            s.send(bytes(str(time.time()).encode()))
+            sum += (time.time() - float(s.recv(1024).decode("utf-8"))) * 1000
+        s.close()
+        print(f'average delay of {count} tries: {sum / count:.03f}ms')
 
 if sys.argv[1] == 'server':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', 8000))
+        s.bind(('0.0.0.0', PORT))
         s.listen()
         connect, client_addr = s.accept()
-        connect.send(connect.recv(1024))
+        for i in range(count):
+            connect.send(connect.recv(1024))
+        connect.close()
